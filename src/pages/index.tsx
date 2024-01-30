43b839293c42b812a83af75e9badca60ee90cc19
Miguel Roldán <mirole16@gmail.com>
2024-01-29 20:03:27 -0500

    Feat: Button functionality and input handling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,50 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
-const Index:FC = () => {
+const Index: FC = () => {
+  const [partyName, setPartyName] = useState<string>("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPartyName(event.target.value);
+  };
+
+  const handleCreateParty = () => {
+    if (!isButtonDisabled) {
+      console.log(partyName);
+    }
+    else{
+      console.log("error");
+    }
+  };
+
+  const isButtonDisabled =
+    partyName.trim() === "" ||
+    !(
+      partyName.length >= 5 &&
+      partyName.length <= 20 &&
+      /^[a-zA-Z0-9 ]+$/.test(partyName) &&
+      (partyName.match(/\d/g) || []).length <= 3 &&
+      isNaN(Number(partyName))
+    );
+
   return (
     <div>
       <img className="party-logo" src="/images/ficha-de-poker.png" />
       <main className="party-container">
         <h3 className="party-container__title-h3">Nombra la partida</h3>
-        <input className="party-container__input" type="text" />
-        <button className="party-container__button">Crear una Partida</button>
+        <input
+          className="party-container__input"
+          type="text"
+          value={partyName}
+          onChange={handleInputChange}
+        />
+        <button
+          className={`party-container__button ${
+            isButtonDisabled ? "party-button__disabled" : ""
+          }`}
+          onClick={handleCreateParty}
+        >
+          Crear una Partida
+        </button>
       </main>
     </div>
   );
