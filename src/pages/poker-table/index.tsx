906d4c66ad932a491a7bffc619ef23a7dacad8d1
Miguel Roldán <mirole16@gmail.com>
2024-02-07 19:19:55 -0500

    Styles: cards and title added

--- src/pages/poker-table/index.tsx
+++ src/pages/poker-table/index.tsx
@@ -54,68 +54,75 @@ const Index = () => {
         </h1>
       </header>
       <main>
-          <div className={styles["top-chairs"]}>top-chairs</div>
-          <div className={styles["desk"]}></div>
-          <div className={styles["left-chair"]}>left-chair</div>
-          <div className={styles["rigth-chair"]}>rigth-chair</div>
-          <div className={styles["botton-chairs"]}>botton-chairs</div>
+        <div className={styles["top-chairs"]}>top-chairs</div>
+        <div className={styles["desk"]}></div>
+        <div className={styles["left-chair"]}>left-chair</div>
+        <div className={styles["rigth-chair"]}>rigth-chair</div>
+        <div className={styles["botton-chairs"]}>{userNameContext}</div>
       </main>
-      <footer>bbb</footer>
+        <div className={styles["card-title"]}>
+          <h2>Elige una carta 👇</h2>
+        </div>
+      <footer>
+        <div className={styles["card-numbers"]}>
+          0
+        </div>
+        <div className={styles["card-numbers"]}>
+          1
+        </div>
+      </footer>
 
       {!blur ? (
-        <div className={`${styles["card__blur"]}`}>
-          <section
-            className={`${styles["container-party"]} ${styles["container-border__glow"]}`}
-          >
-            <h3 className={styles["card-title__h3"]}>Tu nombre</h3>
-            <InputAtom
-              id={userName}
-              type="text"
-              value={userName}
-              onChange={handleInputChange}
-            />
-            <div className={`${styles["card-radioInput__position"]}`}>
-              <label>Jugador</label>
-              <input
-                className={`${styles["card-radioInput"]}`}
-                type="radio"
-                value="jugador"
-                checked={selectedOption === "jugador"}
-                onChange={handleRadioChange}
-              />
-
-              <label style={{ marginLeft: "40px" }}>Espectador</label>
-              <input
-                className={`${styles["card-radioInput"]}`}
-                type="radio"
-                value="espectador"
-                checked={selectedOption === "espectador"}
-                onChange={handleRadioChange}
-              />
-            </div>
-            <ButtonAtom
-              className={`${styles["card-button__size"]} ${
-                validate.length ? styles["card-button__disabled"] : ""
-              }`}
-              onClick={handleCreateParty}
+        <div className={`${styles["general-container"]}`}>
+          <div className={`${styles["card__blur"]}`}>
+            <section
+              className={`${styles["container-party"]} ${styles["container-border__glow"]}`}
             >
-              Crear Partida
-            </ButtonAtom>
-          </section>
+              <h3 className={styles["card-title__h3"]}>Tu nombre</h3>
+              <InputAtom
+                id={userName}
+                type="text"
+                value={userName}
+                onChange={handleInputChange}
+              />
+              <div className={`${styles["card-radioInput__position"]}`}>
+                <label>Jugador</label>
+                <input
+                  className={`${styles["card-radioInput"]}`}
+                  type="radio"
+                  value="jugador"
+                  checked={selectedOption === "jugador"}
+                  onChange={handleRadioChange}
+                />
+
+                <label style={{ marginLeft: "40px" }}>Espectador</label>
+                <input
+                  className={`${styles["card-radioInput"]}`}
+                  type="radio"
+                  value="espectador"
+                  checked={selectedOption === "espectador"}
+                  onChange={handleRadioChange}
+                />
+              </div>
+              <ButtonAtom
+                className={`${styles["card-button__size"]} ${
+                  validate.length ? styles["card-button__disabled"] : ""
+                }`}
+                onClick={handleCreateParty}
+              >
+                Crear Partida
+              </ButtonAtom>
+            </section>
+          </div>
         </div>
       ) : (
         <></>
       )}
-      {/* <div className={styles["map-message__position"]}>
+      <div className={styles["map-message__position"]}>
         {validate.map((message, index) => (
           <p key={index}>{message}</p>
         ))}
       </div>
-      <section>
-        <div className={styles["table1"]}></div>
-        <div className={styles["table2"]}></div>
-        <div className={styles["table3"]}></div>
-      </section> */}
     </div>
   );
 };
