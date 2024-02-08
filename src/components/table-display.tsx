365f2acfa704603e06e4bf71842b699d902a379f
Miguel Roldán <mirole16@gmail.com>
2024-02-08 15:00:31 -0500

    Style: Conditionally displaying user cards

--- src/components/table-display.tsx
+++ src/components/table-display.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
-import styles from "../styles/components/poker-table.module.scss"
-import styles2 from "../styles/components/user-card.module.scss"
+import styles from "../styles/components/poker-table.module.scss";
+import styles2 from "../styles/components/user-card.module.scss";
 import { useUserContext } from "@/hooks/useUserContext";
 import Usercard from "@/system-design/atoms/user-card";
 
@@ -10,9 +10,8 @@ const TableDisplay = () => {
   const router = useRouter();
   const { partyName } = router.query;
   const { rolConText, userNameContext } = useUserContext();
-  const adminList = []
-  
-  
+  const adminList = [];
+
   return (
     <div className={`${styles["table-container"]}`}>
       <header>
@@ -30,15 +29,22 @@ const TableDisplay = () => {
         </button>
       </header>
       <main>
-        <div className={styles["top-chairs"]}>top-chairs</div>
         <div className={styles["desk"]}></div>
         <div className={styles["desk2"]}></div>
         <div className={styles["desk3"]}></div>
-        <div className={styles["left-chair"]}>left-chair</div>
-        <div className={styles["rigth-chair"]}>rigth-chair</div>
-        <div className={styles["botton-chairs"]}>
-          <Usercard className={rolConText === "espectador" ? styles2["card-spectator"] : styles2["card-player"]}>{rolConText === "espectador" ? <p>{userNameContext.slice(0, 2).toUpperCase()}</p> : <></>}</Usercard>
-        </div>
+        {rolConText !== "" ? (
+          <>
+            <div className={styles["top-chairs"]}>top-chairs</div>
+            <div className={styles["left-chair"]}>left-chair</div>
+            <div className={styles["rigth-chair"]}>rigth-chair</div>
+            <div className={styles["botton-chairs"]}>
+              <Usercard
+                className={rolConText === "espectador"? styles2["card-spectator"]: styles2["card-player"]}>
+                {rolConText === "espectador" ? (<p>{userNameContext.slice(0, 2).toUpperCase()}</p>) : null}
+              </Usercard>
+            </div>
+          </>
+        ) : null}
       </main>
       <div className={styles["card-title"]}>
         <h2>Elige una carta 👇</h2>
