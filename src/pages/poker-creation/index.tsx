7e2e80655b873b966ba4710f4e84a181c1f0b604
Miguel Roldán <mirole16@gmail.com>
2024-02-10 11:40:30 -0500

    Fix: Params fixed in url

--- src/pages/poker-creation/index.tsx
+++ src/pages/poker-creation/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import InputAtom from "@/system-design/atoms/input";
 import ButtonAtom from "@/system-design/atoms/button";
 import { useRouter } from "next/router";
-import styles from "../../styles/poker-creation.module.scss";
+import styles from '../../styles/poker-creation.module.scss';
 import usePartyNameValidation from "@/hooks/usePartyNameValidation";
 import { useUserContext } from "@/hooks/useUserContext";
 import { useCreateParty } from "@/hooks/useCreateParty";
@@ -11,7 +11,7 @@ const Index = () => {
   const router = useRouter();
   const [partyName, setPartyName] = useState("");
   const { validate } = usePartyNameValidation(partyName);
-  const { createParty } = useCreateParty();
+  const { createParty} = useCreateParty()
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPartyName(event.target.value);
@@ -19,35 +19,39 @@ const Index = () => {
 
   const handleCreateParty = () => {
     if (!validate.length) {
-      createParty(partyName);
+      //   Petición  
+      createParty(partyName)
+
+      /* router.push(`/poker-table?partyName=${encodeURIComponent(partyName)}`); */
+
     } else {
-      alert("ERROR");
+      alert("ERROR")
     }
   };
 
   return (
     <div>
-      <header className={"upper-container"}>
-        <img className={"party-logo"} src="/images/ficha-de-poker.png" />
-        <h3 className={styles["logo-title"]}>Crear Partida</h3>
+      <header className={'upper-container'}>
+        <img className={'party-logo'} src="/images/ficha-de-poker.png" />
+        <h3 className={styles['logo-title']}>Crear Partida</h3>
       </header>
-      <main className={styles["modal"]}>
-        <h3 className={styles["modal-title"]}>Nombra la partida</h3>
-        <InputAtom
+      <main className={styles['modal']}>
+        <h3 className={styles['modal-title']}>Nombra la partida</h3>
+        <InputAtom 
           id={partyName}
           type="text"
           value={partyName}
           onChange={handleInputChange}
         />
         <ButtonAtom
-          className={`${styles["modal-button"]} ${
-            validate.length ? styles["modal-button__disabled"] : ""
+          className={`${styles['modal-button']} ${
+            validate.length ? styles['modal-button__disabled'] : ""
           }`}
           onClick={handleCreateParty}
         >
           Crear Partida
         </ButtonAtom>
-        <div className={styles["validate-message"]}>
+        <div className={styles['validate-message']}>
           {validate.map((message, index) => (
             <p key={index}>{message}</p>
           ))}
