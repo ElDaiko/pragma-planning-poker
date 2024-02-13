9cc0efb109ba615cc5d7928020738565cd0a5497
Miguel Roldán <mirole16@gmail.com>
2024-02-13 16:54:06 -0500

    Feat: Changes in the style of user voting cards

--- src/system-design/atoms/user-card.tsx
+++ src/system-design/atoms/user-card.tsx
@@ -5,12 +5,13 @@ import { usePartyContext } from "@/hooks/usePartyContext";
 
 export default function Usercard({ className }: any) {
   const { playersList } = usePartyContext();
+  
 
   return (
     <>
       {playersList.map((user) => (
         <div key={user._id} className={`${styles["card"]}`}>
-          <div className={user.type === "spectador" ? styles2["card__spectator"] : styles2["card__player"]}>
+          <div className={user.type === "spectador" ? styles2["card__spectator"] : (user.vote ? styles2["card__playerVote"] : styles2["card__player"])}>
             {user.type === "spectador" ? (
               <p>{user.username.slice(0, 2).toUpperCase()}</p>
             ) : null}
