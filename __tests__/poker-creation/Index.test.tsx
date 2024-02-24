03e015101c007038b6d246cc1be2bfaea60f5064
Miguel Roldán <mirole16@gmail.com>
2024-02-24 18:28:06 -0500

    test: first test applied to ButtonAtom

--- __tests__/poker-creation/Index.test.tsx
+++ __tests__/poker-creation/Index.test.tsx
@@ -3,5 +3,7 @@ import "@testing-library/jest-dom"
 /* import Index from '@/pages/poker-creation' */
 
 describe('hello', () =>{
-    it('hello', () =>{})
+    it('hello', () =>{
+        expect(1).toBe(1)
+    })
 })
---
+++ __tests__/system.design/atoms/ButtonAtom.test.tsx
@@ -0,0 +1,26 @@
+// Importa las bibliotecas necesarias
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ButtonAtom from '@/system-design/atoms/button';
+
+// Realiza las pruebas
+describe('ButtonAtom', () => {
+  it('renders without crashing', () => {
+    render(<ButtonAtom />);
+    // Verifica que el componente se renderice sin errores
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  it('applies custom className', () => {
+    render(<ButtonAtom className="custom-class" />);
+    // Verifica que el className personalizado se aplique correctamente
+    expect(screen.getByRole('button')).toHaveClass('custom-class');
+  });
+
+  it('applies additional props', () => {
+    render(<ButtonAtom data-testid="custom-testid" />);
+    // Verifica que las propiedades adicionales se apliquen correctamente
+    expect(screen.getByTestId('custom-testid')).toBeInTheDocument();
+  });
+});
