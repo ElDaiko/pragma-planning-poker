787840aee5ebdf91b463ce9dae51a6548f5accc2
Miguel Roldán <mirole16@gmail.com>
2024-02-24 17:22:35 -0500

    Chore: Jest configuration working porperly

---
+++ __tests__/poker-creation/Index.test.tsx
@@ -0,0 +1,7 @@
+import {render, screen} from '@testing-library/react'
+import "@testing-library/jest-dom"
+/* import Index from '@/pages/poker-creation' */
+
+describe('hello', () =>{
+    it('hello', () =>{})
+})
