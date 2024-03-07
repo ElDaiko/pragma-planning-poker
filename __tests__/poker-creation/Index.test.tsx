954fe09a8e5fb186475835e2206aeef3be4d548a
Miguel Roldán <mirole16@gmail.com>
2024-03-06 21:26:24 -0500

    test: Index tested 90%

--- __tests__/poker-creation/Index.test.tsx
+++ __tests__/poker-creation/Index.test.tsx
@@ -1,9 +1,43 @@
-import {render, screen} from '@testing-library/react'
-import "@testing-library/jest-dom"
-/* import Index from '@/pages/poker-creation' */
+window.setImmediate = window.setTimeout as any;
 
-describe('hello', () =>{
-    it('hello', () =>{
-        expect(1).toBe(1)
-    })
-})
+import { render, screen, fireEvent } from "@testing-library/react";
+import { useCreateParty } from "../../src/hooks/useCreateParty";
+import Index from "../../src/pages/poker-creation";
+import { PartyProvider } from "../../src/context/party.context";
+import { UserProvider } from "@/context/user.context";
+
+// Mock useRouter
+const pushMocked = jest.fn();
+jest.mock("next/router", () => ({
+  useRouter: () => ({
+    push: pushMocked,
+  }),
+}));
+
+
+jest.mock("../../src/hooks/useCreateParty");
+const jestFn = jest.fn();
+
+describe("useCreateClassroom hook tests", function () {
+  it("Should call function hook", () => {
+    const mockParty = useCreateParty as jest.MockedFunction<
+      typeof useCreateParty
+    >;
+    mockParty.mockReturnValue({
+      createParty: jestFn,
+    });
+
+    render(
+      <UserProvider>
+        <PartyProvider>
+          <Index />
+        </PartyProvider>
+      </UserProvider>
+    );
+
+    fireEvent.input(screen.getByRole("input"), {target:{value: "Sprint32"}})
+    fireEvent.click(screen.getByRole("click"));
+
+    expect(jestFn).toHaveBeenCalledTimes(1);
+  });
+});
