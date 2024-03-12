a3db09118cdbcdc734d4a8fad728e19bd589b82a
Miguel Roldán <mirole16@gmail.com>
2024-03-12 17:26:41 -0500

    fix: socket io using hosted url

--- src/context/party.context.tsx
+++ src/context/party.context.tsx
@@ -9,7 +9,7 @@ import { AmountOfVotes } from "@/types/amountVotes";
 
 export const PartyContext = createContext<partyContextType | null>(null);
 
-const socket = io("http://localhost:8080", {
+const socket = io("https://pragma-websocket-server.onrender.com", {
   reconnection: false  //El usuario no se reconecta automatimaticamente
 })
 
