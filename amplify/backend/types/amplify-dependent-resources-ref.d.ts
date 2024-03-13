4549d0ccce766c9b84b6fca173a84ed568096f72
Miguel Roldán <mirole16@gmail.com>
2024-03-13 10:43:03 -0500

    feat: amplify cli and auth with cognito

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "planningpoker12132eed": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,24 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { UserProvider } from "../context/user.context";
 import { PartyProvider } from "@/context/party.context";
+import config from '../amplifyconfiguration.json';
+import { Amplify } from 'aws-amplify';
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+Amplify.configure(config);
 
-export default function App({ Component, pageProps }: AppProps) {
+
+function MyApp({ Component, pageProps, user, signOut }: AppProps & WithAuthenticatorProps) {
   return (
     <PartyProvider>
       <UserProvider>
         <Component {...pageProps} />
+        <button onClick={signOut}>Sign out</button>
       </UserProvider>
     </PartyProvider>
   );
 }
+
+export default withAuthenticator(MyApp);
+
