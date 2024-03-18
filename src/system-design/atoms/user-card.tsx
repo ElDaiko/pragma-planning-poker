069710173aaac8bfddbdae33b65fb1868d388f2e
Miguel Roldán <mirole16@gmail.com>
2024-03-18 11:02:55 -0500

    Feat: revealing cards in de userCard

--- src/system-design/atoms/user-card.tsx
+++ src/system-design/atoms/user-card.tsx
@@ -3,19 +3,19 @@ import styles from "../../styles/components/user-card.module.scss";
 import { usePartyContext } from "@/hooks/usePartyContext";
 
 export default function Usercard() {
-  const { playersList, owners, socket, isOwner} = usePartyContext();
+  const { playersList, owners, socket, isOwner, revealCards} = usePartyContext();
 
   function giveAdmin(socketID:any) {
     /* console.log("estoy entrando"); */
     socket.emit("add-admin", { socketID })
 }
 
-
   return (
     <>
       {playersList.map((user) => (
         <div role={user.type === "spectador" ? "spectadorCard" : "playerCard"} onClick={isOwner ? () => giveAdmin(user.socketID) : undefined} key={user._id} className={`${styles["card"]}`}>
-          <div className={user.type === "spectador" ? styles["card__spectator"] : (user.vote ? styles["card__playerVote"] : styles["card__player"])}>
+          <div className={user.type === "spectador" ? styles["card__spectator"] : (!revealCards && user.vote ? styles["card__playerVote"] : styles["card__player"])}>
+          {revealCards && <p>{user.vote}</p>}
             {user.type === "spectador" ? (
               <p>{user.username.slice(0, 2).toUpperCase()}</p>
             ) : null}
