1d81008b396b90c3018ce0a5ffa5e8de697fccb6
Miguel Roldán <mirole16@gmail.com>
2024-03-18 15:30:19 -0500

    feat: loader while counting cards added

--- src/pages/poker-table/[id]/components/table-display.tsx
+++ src/pages/poker-table/[id]/components/table-display.tsx
@@ -5,15 +5,23 @@ import { usePartyContext } from "@/hooks/usePartyContext";
 import Footer from "./footer";
 import InvitationModal from "./invitation-modal";
 import Header from "./header";
+import CardLoader from "./cardLoader";
 
 const TableDisplay = () => {
-  const {socket, isOwner, allNonSpectatorVoted, invitationBlur } =
+  const { socket, isOwner, allNonSpectatorVoted, invitationBlur } =
     usePartyContext();
   const [reset, setReset] = useState(false);
+  const [loadingVotes, setLoadingVotes] = useState(Boolean);
 
   function handleRevealCards() {
-    socket.emit("reveal-cards");
+    setLoadingVotes(true);
+    setTimeout(() => {
+      socket.emit("reveal-cards");
+      setLoadingVotes(false);
+    }, 3000);
   }
+
+  console.log(loadingVotes);
 
   function handleResetMatch() {
     socket.emit("reset-classroom");
@@ -21,7 +29,7 @@ const TableDisplay = () => {
 
   return (
     <>
-    {invitationBlur ? <InvitationModal /> : <></>}
+      {invitationBlur ? <InvitationModal /> : <></>}
       <div className={`${styles["container"]}`}>
         <Header></Header>
         <main>
@@ -29,19 +37,31 @@ const TableDisplay = () => {
           <div className={styles["container__desk2"]}></div>
           <div className={styles["container__desk3"]}>
             {isOwner && allNonSpectatorVoted && !reset ? (
+              <>
+                {!loadingVotes && (
+                  <button
+                    onClick={() => {
+                      handleRevealCards();
+                      setReset(true);
+                    }}
+                    className={styles["container__button-reveal"]}
+                  >
+                    Revelar Cartas
+                  </button>
+                )}
+              </>
+            ) : reset && !loadingVotes ? (
               <button
-                onClick={() => {handleRevealCards(); setReset(true)}}
-                className={styles["container__button-reveal"]}
-              >
-                Revelar Cartas
-              </button>
-            ) : reset ? (
-              <button
-                onClick={() => {handleResetMatch(); setReset(false)}}
+                onClick={() => {
+                  handleResetMatch();
+                  setReset(false);
+                }}
                 className={styles["container__button-reveal"]}
               >
                 Nueva votación
               </button>
+            ) : loadingVotes ? (
+              <CardLoader></CardLoader>
             ) : (
               <></>
             )}
